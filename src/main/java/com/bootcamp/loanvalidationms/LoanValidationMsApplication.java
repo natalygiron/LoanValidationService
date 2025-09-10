ed2a23173646d206ee9f1cd56fb9d0ffa8e104ce
nataly <natalygiron18@outlook.com>
2025-09-10 16:32:21 -0500

    init: base project

---
+++ src/main/java/com/bootcamp/loanvalidationms/LoanValidationMsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.loanvalidationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanValidationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanValidationMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp/loanvalidationms/LoanValidationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.loanvalidationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanValidationMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
